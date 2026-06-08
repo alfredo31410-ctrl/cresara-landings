d5569449fd32e3f16cbbd4d70a0a7d7831b65dae
JohnBecerra16 <cardonabecerrajonathanmanuel@gmail.com>
2026-06-08 10:05:44 -0600

    Cargar assets Next desde el proyecto de landings

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://cresara-landings.vercel.app"
+      : undefined,
   images: {
     unoptimized: true,
   },
